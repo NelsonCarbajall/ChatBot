493f8569172f59258c2cec2a9888adf87c167731
Nelson Carbajal <nelsitocarbajal@gmail.com>
2024-08-07 14:26:04 -0300

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -8,7 +8,7 @@ genai.configure(api_key=chave_key)
 
 modelo = genai.GenerativeModel("gemini-1.5-pro-latest")
 
-print("E ai meu parceiro, Aqui e seu amigo NelBot!")
+print("E ai Amigo, Aqui e seu amigo NelBot!")
 print("Para sair escreva 'sair'", "\n")
 
 while True:
